aa76ab798bea8b6b56026a6ab0f800ae322d388d
Crispin Rebuelta <crispin.rebuelta@icloud.com>
2022-01-21 11:36:33 +0800

    Updated code

--- app/Main.hs
+++ app/Main.hs
@@ -16,14 +16,10 @@ main = do {
     args <- getArgs;
     (env,status) <- return (parseArgs args);
 
-    -- Implementation Notes --
-    -- To make it easier to run, we can simply hard code the arguments here
-    -- (env,status) <- return (parseArgs ["10", "10", "4", "3", "1", "1"]);
-
-    -- This clear the original content in the log file
+    -- This clears the original content in the log file
     writeFile "log.txt" [];
 
-    -- This will loop indefinitely
+    -- This will loop indefinitely until terminated by user
     loop env status;
 
     return ();
@@ -31,13 +27,11 @@ main = do {
 
 loop :: Env -> Status -> IO ()
 loop env status = do {
-    -- Implementation Notes --
-    -- Here is the heart of the transformation to use MonadTransformer
-    -- Note that the frameCount is hard coded to 1 here, 
+   
+    -- This is where MonadTransformers are used
     ((_,newStatus),log) <- runWriterT (runStateT (runReaderT (runAnimation 1) env) status);
 
-    -- Implementation Notes --
-    -- The writer monad accumulated some log messages, time to save it to a file
+    -- Writer monad accumulates the log messages then saves it to a file "log.txt"
     appendFile "log.txt" log;
     loop env newStatus;
     return ()
